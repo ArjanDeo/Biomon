0c00453090067829c580cfab412c8d0744e9eac1
Arjan Deo <arjandeo@bluecomet-it.com>
2026-08-18 03:15:48 +0100

    Initial Commit

---
+++ Biomon/BiomonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiomonApp.swift
+//  Biomon
+//
+//  Created by Arjan Deo on 18/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BiomonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
